c1068e9bc98319f86a21a54f2ac2f262e73ed224
Petar Ivanov <petlv@mail.bg>
2020-01-13 01:43:00 +0200

    Firebase API in signup form

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,6 +1,7 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
-const config = {
+const devConfig = {
     apiKey: "AIzaSyBBvo2Vmhm-GLCtzkhaz9vnAFhoH7GpPqk",
     authDomain: "hotelcomments-f0e49.firebaseapp.com",
     databaseURL: "https://hotelcomments-f0e49.firebaseio.com",
@@ -11,36 +12,46 @@ const config = {
     measurementId: "G-56LHL2WG61"
 };
 
+const prodConfig = {
+   /* apiKey: process.env.REACT_APP_PROD_API_KEY,
+    authDomain: process.env.REACT_APP_PROD_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_PROD_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROD_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_PROD_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_PROD_MESSAGING_SENDER_ID,*/
+};
+
+/*const devConfig = {
+    apiKey: process.env.REACT_APP_DEV_API_KEY,
+    authDomain: process.env.REACT_APP_DEV_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DEV_DATABASE_URL,
+    projectId: process.env.REACT_APP_DEV_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_DEV_MESSAGING_SENDER_ID,
+};*/
+
+const config =
+    process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+
 class Firebase {
     constructor() {
         app.initializeApp(config);
+
+        this.auth = app.auth();
     }
+
+    // *** Auth API ***
+    doCreateUserWithEmailAndPassword = (email, password) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) =>
+        this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password =>
+        this.auth.currentUser.updatePassword(password);
 }
 export default Firebase;
-
-/*
-import app from 'firebase/app';
-const prodConfig = {
-  apiKey: process.env.REACT_APP_PROD_API_KEY,
-  authDomain: process.env.REACT_APP_PROD_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_PROD_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROD_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_PROD_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_PROD_MESSAGING_SENDER_ID,
-};
-const devConfig = {
-  apiKey: process.env.REACT_APP_DEV_API_KEY,
-  authDomain: process.env.REACT_APP_DEV_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DEV_DATABASE_URL,
-  projectId: process.env.REACT_APP_DEV_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_DEV_MESSAGING_SENDER_ID,
-};
-const config =
-  process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
-class Firebase {
-  constructor() {
-    app.initializeApp(config);
-  }
-}
-export default Firebase;*/
